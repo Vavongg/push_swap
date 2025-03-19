86b8b19afed3dcae472af1572809e971d8eb3a57
Alexis Inthanavong <ainthana@bess-f4r5s8.clusters.42paris.fr>
2025-03-19 16:46:29 +0100

    update turk_sort 19/03

--- algorithmes/costs.c
+++ algorithmes/costs.c
@@ -6,10 +6,18 @@
 /*   By: ainthana <ainthana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 15:57:32 by ainthana          #+#    #+#             */
-/*   Updated: 2025/03/19 15:57:55 by ainthana         ###   ########.fr       */
+/*   Updated: 2025/03/19 16:46:17 by ainthana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
+int	get_cost_a(t_stack *a)
+{
 
+}
+
+int	get_cost_b(t_stack *b)
+{
+
+}
